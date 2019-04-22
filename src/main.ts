f18c222484d15c6646a40b979becf6744080b99f
Do Thanh Phong <phongdt@mti-tech.vn>
2019-04-22 16:33:25 +0700

    add snapsvg

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+// const Snap = require('snapsvg/dist/snap.svg.js');
+const Snap = require(`imports-loader?this=>window,fix=>module.exports=0!snapsvg/dist/snap.svg-min.js`);
+
+(window as any).Snap = Snap;
+
 Vue.config.productionTip = false;
 
 new Vue({
